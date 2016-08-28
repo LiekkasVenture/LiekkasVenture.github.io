c05b704b7ee5c02937392198fc5c5d478a38577e
Batur <heshupai@gmail.com>
2016-08-28 12:51:14 +0800

    change style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.jpg';
 import './App.css';
-//import ReactMarkDown from 'react-markdown';
 var ReactMarkdown = require('react-markdown');
 
-var SammyWord = '> Sammy \n```Liekkas，一首来自北欧萨米族的歌曲，寓意北极光的征兆。\n   在忙碌的深圳，感动于它传递的暖意。\n   我们以Liekkas Venture命名英语角，延续这份暖意。\n   在这小小的角落寄托了我们美好的期许。\n   希望未来有一天，我们来一场北极光的冒险之旅！';
+var SammyWord = '## Sammy\n```\nLiekkas，一首来自北欧萨米族的歌曲，\n寓意北极光的征兆。\n在忙碌的深圳，\n 感动于它传递的暖意。\n我们以Liekkas Venture命名英语角，\n 延续这份暖意。\n在这小小的角落寄托了我们美好的期许。\n希望未来有一天，我们来一场北极光的冒险之旅！\n```';
 
-var BaturWord = '> Batur \n```这是一个实验，\n   两个有趣的人尝试聚集更多有趣的人，\n   一起做一些天马行空的事。\n   梦幻的北极光那么遥远，\n   这两个蜗牛开始了漫漫征途。\n   加入我们吧!';
+var BaturWord = '## Batur \n```这是一个实验，\n两个有趣的人尝试聚集更多有趣的人，\n一起做一些天马行空的事。\n梦幻的北极光那么遥远，\n这两个蜗牛开始了漫漫征途。\n加入我们吧!\n```';
 
-var input = '# This is a header\n\nAnd this is a paragraph';
 
 class App extends Component {
   render() {
@@ -18,8 +16,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Liekkas Venture</h2>
         </div>
-        <ReactMarkdown source={SammyWord} />
-        <ReactMarkdown source={BaturWord} />
+		<div className="Words">
+			<ReactMarkdown source={SammyWord} />
+			<ReactMarkdown source={BaturWord} />
+		</div>
       </div>
     );
   }
